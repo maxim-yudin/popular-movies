95d035376f19c8b419c64ea9021ad4d8d466260d
Maxim Yudin <maxim.yudin@gmail.com>
2015-06-25 08:16:53 +0300

    Mini fix

--- app/src/main/java/jqsoft/ru/nanodegree/popularmoviesapp/MovieDbApi.java
+++ app/src/main/java/jqsoft/ru/nanodegree/popularmoviesapp/MovieDbApi.java
@@ -6,7 +6,7 @@ import retrofit.RestAdapter;
  * Created by maximyudin on 25.06.15.
  */
 public class MovieDbApi {
-    public static final String API_BASE_URL = "http://api.themoviedb.org/3";
+    private static final String API_BASE_URL = "http://api.themoviedb.org/3";
     private final MovieDbService mMovieDbService;
 
     public MovieDbApi() {
